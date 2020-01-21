b26f7af07017ed8b58e16711e2f3dea7615b9095
jmfdiazAL <josemanuel.fernandez@iesalandalus.org>
2020-01-21 13:41:12 +0100

    21/01/2020

---
+++ Práctica 3.3/WebSocket2/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const socketio = require('socket.io');
+
+const server = http.createServer((req, res) => {
+    res.end("I am connected");
+});
+const io = new socketio(server);
+
+io.on('connection', (socket, req) => {
+    socket.emit('messageFromServer', 'This is a message from server, connection is established');
+    socket.on('messageFromClient', (msg) => {
+        console.log(msg);
+    });
+});
+server.listen(8000);
